215fb9eee8ac5d7e771e40f4423e42d99e1b8920
Sunil <52912915+sunilskcj@users.noreply.github.com>
2022-03-20 01:10:24 +0530

    init

---
+++ Banking.BusinessModels/AccountModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking.BusinessModels
+{
+    internal class AccountModel
+    {
+    }
+}
---
+++ NetBanking.AccountServiceApi/Controllers/AccountsController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using NetBanking.DataAccessLayer.Repository.Abstraction;
+using NetBanking.BusinessModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetBanking.AccountServiceApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private IAccountFieldDao _accountFieldDao;
+        
+
+        public AccountsController(IAccountFieldDao accountFieldDao)
+        {
+            _accountFieldDao = accountFieldDao;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllAccountDetails()
+        {
+
+            var fetchedData = _accountFieldDao.FetchAllAccount();
+            return this.Ok(fetchedData);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult FetchAccountById(int id)
+        {
+
+            var fetchedData = _accountFieldDao.FetchAccountById(id);
+            return this.Ok(fetchedData);
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(AccountModel account)
+        {
+
+            var result = _accountFieldDao.InsertAccountField(account);
+            return this.CreatedAtAction(
+                "AddProduct",
+                new
+                {
+                    StatusCode = 201,
+                    Response = result,
+                    Data = account
+                }
+                );
+        }
+
+
+        [HttpPut]
+        [Route("{customerId}")]
+        public IActionResult UpdateProduct(AccountModel account, int customerId)
+        {
+            var result = _accountFieldDao.UpdateAccountField(account, customerId);
+           
+            return this.CreatedAtAction(
+                "UpdateProduct",
+                new
+                {
+                    StatusCode = 201,
+                    Response = result,
+                    
+                    Data = account
+                }
+                ) ;
+        }
+
+        [HttpDelete]
+        [Route("{customerId}")]
+        public IActionResult DeleteProduct(int customerId)
+        {
+            var result = _accountFieldDao.DeleteAccountFieldbyID(customerId);
+            return this.CreatedAtAction(
+                "DeleteProduct",
+                new
+                {
+                    StatusCode = 201,
+                    Response = result,
+
+                }
+                );
+        }
+
+
+
+
+ 
+    }
+
+}
